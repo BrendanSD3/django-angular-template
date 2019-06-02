bda923bff4b5831ecc2347e69bfa1a54cd02047d
bd1887 <bd1887@gmail.com>
2019-06-02 16:36:38 +0100

    init commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageTwoComponent } from './page-two/page-two.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'page-one', component: PageOneComponent },
+  { path: 'page-two', component: PageTwoComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageNotFoundComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
